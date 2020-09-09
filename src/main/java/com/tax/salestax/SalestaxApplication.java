2e2eac40f8c063a025a61fba99f64e6fd33cfab7
Parisa Nikzad <parisa.nikzad@yahoo.com>
2020-09-08 21:05:32 -0400

    closing the resource + improving exceptions handling

--- src/main/java/com/tax/salestax/SalestaxApplication.java
+++ src/main/java/com/tax/salestax/SalestaxApplication.java
@@ -13,7 +13,7 @@ import java.io.InputStreamReader;
 
 public class SalestaxApplication {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 
 		CommandExecutor commandExecutor = CommandExecutor.getInstance();
 		Receipt receipt = Receipt.getInstances();
@@ -22,18 +22,30 @@ public class SalestaxApplication {
 		bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
 		while (true) {
-			String commandText = bufferedReader.readLine();
-			if (commandText.isEmpty() || "exit".equalsIgnoreCase(commandText)) {
-				System.out.println(receipt.getReceipt());
-				break;
-			} else {
-				// if execution is not success then exit
-				boolean executionSuccess = commandExecutor.execute(commandText);
-				if(!executionSuccess) {
+			try{
+				String commandText = bufferedReader.readLine();
+				if (commandText.isEmpty() || "exit".equalsIgnoreCase(commandText)) {
+					System.out.println(receipt.getReceipt());
 					break;
+				} else {
+					// if execution is not success then exit
+					boolean executionSuccess = commandExecutor.execute(commandText);
+					if(!executionSuccess) {
+						break;
+					}
+				}
+			}catch (Exception e){
+				System.out.println(e);
+			}
+			finally{
+				try{
+					bufferedReader.close();
+				}catch(Exception e){
+					System.out.println(e);
 				}
 			}
 		}
+
 	}
 
 }
